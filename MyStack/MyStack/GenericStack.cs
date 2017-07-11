84ab036e3fdaae3a5e89e8b013396aa526aaba58
okocsis90 <o.kocsis90@gmail.com>
2017-07-11 10:46:30 +0200

    Stack implemented with tests, all tests are OK

---
+++ MyStack/MyStack/GenericStack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace MyStack
+{
+    public class GenericStack<T>
+    {
+        public int Length { get; set; }
+        public int FreeSpaces { get; set; }
+
+        private T pointer;
+        private T[] TArray;
+
+        public GenericStack(int length)
+        {
+            TArray = new T[length];
+            Length = 0;
+            FreeSpaces = length;
+        }
+
+        public void Push(T elem)
+        {
+            if (FreeSpaces == 0)
+            {
+                throw new StackOverflowException("Stack is full. Please remove an element to add one.");
+            }
+            TArray[Length] = elem;
+            pointer = elem;
+            Length++;
+            FreeSpaces--;
+        }
+
+        public T Pop()
+        {
+            if (Length == 0)
+            {
+                throw new Exception("Stack is empty, cannot Pop any elements.");
+            }
+            T result = pointer;
+            Length--;
+            FreeSpaces++;
+            pointer = TArray[Length];
+            return result;
+        }
+
+        public T Peek()
+        {
+            return pointer;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = 0; i < Length; i++)
+            {
+                result += " " + TArray[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ MyStack/MyStackTests/GenericStackTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack.Tests
+{
+    [TestClass()]
+    public class GenericStackTests
+    {
+        [TestMethod()]
+        public void GenericStackTest()
+        {
+            GenericStack<int> myStack = new GenericStack<int>(3);
+            Assert.IsNotNull(myStack);
+        }
+
+        [TestMethod()]
+        public void PushTest_TestValidInput()
+        {
+            GenericStack<int> myStack = CreateIntStack();
+            Assert.AreEqual(" 1 2 3", myStack.ToString());
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(StackOverflowException), "Stack is full. Please remove an element to add one.")]
+        public void PushTest_ThrowsExceptionWhenStackIsFull()
+        {
+            GenericStack<int> myStack = CreateIntStack();
+            myStack.Push(4);
+        }
+
+        [TestMethod()]
+        public void PopTest_PoppedElementIsLast()
+        {
+            GenericStack<int> myStack = CreateIntStack();
+            int popped = myStack.Pop();
+            Assert.AreEqual(3, popped);
+        }
+
+        [TestMethod()]
+        public void PopTest_PoppedElementIsRemoved()
+        {
+            GenericStack<int> myStack = CreateIntStack();
+            myStack.Pop();
+            Assert.AreEqual(" 1 2", myStack.ToString());
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception), "Stack is empty, cannot Pop any elements.")]
+        public void PopTest_ThrowsExceptionWhenLengthIsZero()
+        {
+            GenericStack<int> myStack = CreateIntStack();
+            myStack.Pop();
+            myStack.Pop();
+            myStack.Pop();
+            myStack.Pop();
+        }
+
+        [TestMethod()]
+        public void PeekTest_LastElementIsShown()
+        {
+            GenericStack<int> myStack = CreateIntStack();
+            int lastElem = myStack.Peek();
+            Assert.AreEqual(3, lastElem);
+        }
+
+        [TestMethod()]
+        public void PeekTest_LastElementIsNotRemoved()
+        {
+            GenericStack<int> myStack = CreateIntStack();
+            int lastElem = myStack.Peek();
+            Assert.AreEqual(" 1 2 3", myStack.ToString());
+        }
+
+        public GenericStack<int> CreateIntStack()
+        {
+            GenericStack<int> myStack = new GenericStack<int>(3);
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            return myStack;
+        }
+    }
+}
